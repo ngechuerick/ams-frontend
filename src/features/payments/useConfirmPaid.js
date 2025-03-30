0f3709efd18d27391bd4329d9eae9f51078b4a89
Rickyngechu <ekngechu003@gmail.com>
2025-03-31 02:27:09 +0300

    paid fix

--- src/features/payments/useConfirmPaid.js
+++ src/features/payments/useConfirmPaid.js
@@ -1,16 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
-import { confirmPayment } from '../../services/apiPayment';
+// import { useMutation } from '@tanstack/react-query';
+// import { confirmPayment } from '../../services/apiPayment';
 
-export function useConfirmPaid() {
-  const { mutate: confirmPay, isPending } = useMutation({
-    mutationFn: confirmPayment,
-    onSuccess: response => {
-      console.log(response);
-    },
-    onError: err => {
-      console.log(err);
-    },
-  });
+// export function useConfirmPaid() {
+//   const { mutate: confirmPay, isPending } = useMutation({
+//     mutationFn: confirmPayment,
+//     onSuccess: response => {
+//       console.log(response);
+//     },
+//     onError: err => {
+//       console.log(err);
+//     },
+//   });
 
-  return { confirmPay, isPending };
-}
+//   return { confirmPay, isPending };
+// }
